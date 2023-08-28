d8abc8e60cafd046142826008592650e5ad84253
mohamed zaitoune <115373788+ZaitouneMohamed@users.noreply.github.com>
2023-08-28 14:46:46 +0100

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\Admin\CategorieController;
+use App\Http\Controllers\Auth\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -16,4 +18,17 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::apiResource("categorie", CategorieController::class);
+
+Route::controller(AuthController::class)->group(function () {
+    Route::middleware("guest")->group(function () {
+        Route::post("register", "register");
+        Route::post("login", "login");
+    });
+    Route::middleware('auth:sanctum')->group(function () {
+        Route::get("profile", "profile");
+        Route::post("logout", "logout");
+    });
 });
