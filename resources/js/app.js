e5d0dbeab3c88528e56c906f85ecf373d1df133c
mohamed zaitoune <115373788+ZaitouneMohamed@users.noreply.github.com>
2023-08-28 20:56:42 +0100

    add vue js

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,10 @@
-require('./bootstrap');
+require('./bootstrap')
+
+import { createApp } from 'vue'
+import Welcome from './components/index.vue'
+
+const app = createApp({})
+
+app.component('welcome', Welcome)
+
+app.mount('#app')
