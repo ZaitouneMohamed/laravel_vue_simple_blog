27e38ca45d135f819bfc968ac11f6d8e0c967f9a
mohamed zaitoune <115373788+ZaitouneMohamed@users.noreply.github.com>
2023-08-31 22:07:53 +0100

    Update Post.php

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -20,5 +21,13 @@ class Post extends Model
     public function Categorie()
     {
         return $this->belongsTo(Categorie::class);
+    }
+    public function Comments()
+    {
+        return $this->hasMany(Comment::class);
+    }
+    public function getCreatedAtAttribute($value)
+    {
+        return Carbon::parse($value)->diffForHumans();
     }
 }
