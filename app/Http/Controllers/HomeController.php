6f67c04dd56e280ecea7fe35e99d2017dad336cf
mohamed zaitoune <115373788+ZaitouneMohamed@users.noreply.github.com>
2023-08-31 22:08:30 +0100

    create controller function

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Comment;
 use App\Models\Post;
+use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
@@ -10,5 +12,14 @@ class HomeController extends Controller
     {
         $posts = Post::where('title', 'LIKE', '%' . $word . '%')->get();
         return response()->json($posts);
+        }
+    public function CreateComment(Request $request, $id)
+    {
+        Comment::create([
+            "post_id" => $id,
+            "user_id" => 2,
+            'body' => "dfghjiopojhg"
+        ]);
+        return response()->json(['message' => 'Comment created successfully']);
     }
 }
