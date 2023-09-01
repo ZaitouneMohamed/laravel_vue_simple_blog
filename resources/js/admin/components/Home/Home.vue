f2c204a8d3c66b0a3b9036192ed5385980c03da9
mohamed zaitoune <115373788+ZaitouneMohamed@users.noreply.github.com>
2023-09-01 21:24:36 +0100

    add home & posts components for admin

---
+++ resources/js/admin/components/Home/Home.vue
@@ -0,0 +1,9 @@
+<template>
+    <div class="content-wrapper">
+        <div class="container">
+            <h1>Home admin pageeeeeeeeeeeeeeeeee</h1>
+        </div>
+    </div>
+</template>
+<script>
+</script>
