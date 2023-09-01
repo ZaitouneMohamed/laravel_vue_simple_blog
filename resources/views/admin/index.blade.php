5335e6cbc336b2c9b90eda525457949380d14115
mohamed zaitoune <115373788+ZaitouneMohamed@users.noreply.github.com>
2023-09-01 19:07:05 +0100

    Update index.blade.php

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -1,19 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
+@extends("admin.layouts.master")
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-
-<body>
-
-    <div id="admin">
-        <AdminApp />
-    </div>
-    <script src="{{ mix('js/admin/admin.js') }}"></script>
-</body>
-
-</html>
+@section("content")
+admin home page
+<AdminApp />
+@endsection
