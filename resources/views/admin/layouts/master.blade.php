29f4dd96592e382b66fc50689d8193160700bcc7
mohamed zaitoune <115373788+ZaitouneMohamed@users.noreply.github.com>
2023-09-01 21:25:33 +0100

    Update master.blade.php

--- resources/views/admin/layouts/master.blade.php
+++ resources/views/admin/layouts/master.blade.php
@@ -17,7 +17,9 @@
     @yield('style')
 
     <script defer src="https://unpkg.com/alpinejs@3.x.x/dist/cdn.min.js"></script>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css" integrity="sha512-SzlrxWUlpfuzQ+pcUCosxcglQRNAq/DZjVsC0lE40xsADsfeQoEypE+enwcOiGjk/bSuGGKHEyjSoQ1zVisanQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css"
+        integrity="sha512-SzlrxWUlpfuzQ+pcUCosxcglQRNAq/DZjVsC0lE40xsADsfeQoEypE+enwcOiGjk/bSuGGKHEyjSoQ1zVisanQ=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
 
     <link rel="stylesheet" href="{{ asset('adminlte/dist/css/adminlte.min.css') }}">
     <!-- Google Font: Source Sans Pro -->
@@ -62,7 +64,7 @@
                         <div class="dropdown-divider"></div>
                         <form method="POST" id="frm">
                             @csrf
-                            @method("post")
+                            @method('post')
                             <button type="submit" class="dropdown-item text-center">log out </button>
                         </form>
                     </div>
@@ -72,74 +74,10 @@
         <!-- /.navbar -->
 
         <!-- Main Sidebar Container -->
-        <aside class="main-sidebar sidebar-dark-primary elevation-4">
-            <!-- Brand Logo -->
-            <a href="index3.html" class="brand-link">
-                <img src="{{ asset('adminlte/dist/img/AdminLTELogo.png') }}" alt="AdminLTE Logo"
-                    class="brand-image img-circle elevation-3" style="opacity: .8">
-                <span class="brand-text font-weight-light">AdminLTE 3</span>
-            </a>
-
-            <!-- Sidebar -->
-            <div class="sidebar">
-                <!-- Sidebar user panel (optional) -->
-                <div class="user-panel mt-3 pb-3 mb-3 d-flex">
-                    <div class="image">
-                        <img src="{{ asset('adminlte/dist/img/user2-160x160.jpg') }}"
-                            class="img-circle elevation-2" alt="User Image">
-                    </div>
-                    <div class="info">
-                        <a href="#" class="d-block">admin</a>
-                    </div>
-                </div>
-                <nav class="mt-2">
-                    <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu"
-                        data-accordion="false">
-                        <li class="nav-item">
-                            <a href="" class="nav-link">
-                                <i class="fa-solid fa-bookmark"></i>
-                                <p>
-                                    Categories
-                                    <span class="right badge badge-success">5</span>
-                                </p>
-                            </a>
-                        </li>
-                        <li class="nav-item">
-                            <a href="" class="nav-link">
-                                <i class="fa-solid fa-user"></i>
-                                <p>
-                                    Sub Categoiries
-                                    <span class="right badge badge-success">
-                                        9
-                                    </span>
-                                </p>
-                            </a>
-                        </li>
-                        <li class="nav-item">
-                            <a href="" class="nav-link">
-                                <i class="fa-solid fa-tag"></i>
-                                <p>
-                                    products
-                                    <span class="right badge badge-success">
-                                        4{{-- {{\App\Models\Tags::all()->count()}} --}}
-                                    </span>
-                                </p>
-                            </a>
-                        </li>
-                    </ul>
-                </nav>
-                <!-- /.sidebar-menu -->
-            </div>
-            <!-- /.sidebar -->
-        </aside>
 
         <!-- Content Wrapper. Contains page content -->
-        <div class="content-wrapper">
-            <div class="container">
-                <div id="admin">
-                    @yield('content')
-                </div>
-            </div>
+        <div id="admin">
+            @yield('content')
         </div>
         <!-- REQUIRED SCRIPTS -->
         <script src="{{ mix('js/admin/admin.js') }}"></script>
