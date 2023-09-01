9f1b197c8c849c827968aea024b9f47c763b0d26
mohamed zaitoune <115373788+ZaitouneMohamed@users.noreply.github.com>
2023-09-01 22:42:24 +0100

    Create 404.vue

---
+++ resources/js/errors/404.vue
@@ -0,0 +1,19 @@
+<!-- NotFound.vue -->
+<template>
+    <div>
+        <center>
+            <h1>404 - Page Not Found</h1>
+            <p>The page you are looking for does not exist.</p>
+        </center>
+    </div>
+</template>
+
+<script>
+export default {
+    // Component logic goes here
+};
+</script>
+
+<style scoped>
+/* Add custom styles for the error page */
+</style>
