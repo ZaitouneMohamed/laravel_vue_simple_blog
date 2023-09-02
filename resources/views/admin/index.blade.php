aefcdd4364f9809e55eb148296e28701d0003c30
mohamed zaitoune <115373788+ZaitouneMohamed@users.noreply.github.com>
2023-09-02 15:07:48 +0100

    Update index.blade.php

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -3,3 +3,18 @@
 @section('content')
     <AdminApp />
 @endsection
+
+@section('scripts')
+    <script>
+        var token = localStorage.getItem("token");
+        if (token) {
+            var role = JSON.parse(localStorage.getItem("role"));
+            if (role == "admin") {
+            } else {
+                window.location.replace("http://localhost:8000/");
+            }
+        } else {
+            window.location.replace("http://localhost:8000/");
+        }
+    </script>
+@endsection
