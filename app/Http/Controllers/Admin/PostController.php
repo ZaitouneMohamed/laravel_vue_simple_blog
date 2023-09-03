79dfa4b0130a49ecd74948a09e68be01d83d3314
mohamed zaitoune <115373788+ZaitouneMohamed@users.noreply.github.com>
2023-09-03 21:33:23 +0100

    add pagination in query

--- app/Http/Controllers/Admin/PostController.php
+++ app/Http/Controllers/Admin/PostController.php
@@ -17,7 +17,7 @@ class PostController extends Controller
      */
     public function index()
     {
-        $posts = Post::all();
+        $posts = Post::paginate(6);
         return response()->json($posts);
     }
 
