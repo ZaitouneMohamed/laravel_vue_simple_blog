3b5733ad9f7069da0d48edbeec5d861329f9aabc
mohamed zaitoune <115373788+ZaitouneMohamed@users.noreply.github.com>
2023-09-03 21:32:54 +0100

    get errors pages

---
+++ resources/views/errors/403.blade.php
@@ -0,0 +1,17 @@
+{{-- @extends('errors::minimal') --}}
+
+{{-- @section('title', ('auth login'))
+@section('code', '403 login page')
+@section('message', __($exception->getMessage() ?: 'Forbidden')) --}}
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <h1>hello world</h1>
+</body>
+</html>
